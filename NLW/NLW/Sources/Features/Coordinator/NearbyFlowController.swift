93c5fddfc806c47be34cb500f5b33b3647e9a184
Lucas <souza.8.lucas@gmail.com>
2024-12-12 23:41:14 -0300

    Aula 01

---
+++ NLW/NLW/Sources/Features/Coordinator/NearbyFlowController.swift
@@ -0,0 +1,26 @@
+//
+//  NearbyFlowController.swift
+//  NLW
+//
+//  Created by Lucas Souza on 12/12/24.
+//
+
+import Foundation
+import UIKit
+
+class NearbyFlowController {
+    private var navigationController: UINavigationController?
+    
+    public init(){
+    }
+    
+    func start() -> UINavigationController? {
+        let contentView = SplashView()
+        let startViwerController = SplashViewController(contentView: contentView) //coordinator
+        self.navigationController = UINavigationController(rootViewController: startViwerController)
+        
+        
+        return navigationController
+    }
+    
+}
